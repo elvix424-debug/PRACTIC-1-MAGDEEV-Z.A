79c812af5c35afbe06facbc9aac857394a8892d7
Магдеев <elvix424@gmail.com>
2026-09-11 09:58:34 +0300

    Lab01: variables and interpolation

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string myName = "Заур Магдеева";
+string groupName = "ИСП-254";
+int courseNumber = 2;
+double averageGrade = 4.6;
+bool isBudget = false;
+
+Console.WriteLine("Знакомство");
+Console.WriteLine($"Студент: {myName}");
+Console.WriteLine($"Группа: {groupName}");
+Console.WriteLine($"Курс: {courseNumber}");
+Console.WriteLine($"Средний балл: {averageGrade}");
+Console.WriteLine($"Бюджетное место: {isBudget}");
--- Lab01/obj/Debug/net10.0/Lab01.AssemblyInfo.cs
+++ Lab01/obj/Debug/net10.0/Lab01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa0c9cf2562e3665d73fbfa4afdd8ad86b7f6c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9e09d24971082211c0764c3d8135271c2496a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
