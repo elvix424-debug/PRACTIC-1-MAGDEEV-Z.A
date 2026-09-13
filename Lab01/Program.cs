4c5a51a54507d07c7b13aefa0ff12a8850ba9faf
elvix424-debug <elvix424@gmail.com>
2026-09-13 23:06:44 +0300

    Lab01: arithmetic with variables

--- Lab01/Program.cs
+++ Lab01/Program.cs
@@ -1,4 +1,34 @@
-﻿Console.WriteLine();
+﻿//комната
+Console.WriteLine();
+Console.WriteLine("Ремонт: комната");
+
+double roomWidth = 3.5;
+double roomLength = 4.2;
+
+double roomArea = roomWidth * roomLength;
+double roomPerimeter = (roomWidth + roomLength) * 2;
+
+Console.WriteLine($"Ширина: {roomWidth} м, длина: {roomLength} м");
+Console.WriteLine($"Площадь: {roomArea} кв.м");
+Console.WriteLine($"Периметр: {roomPerimeter}");
+
+//ноутбук
+Console.WriteLine();
+Console.WriteLine("Покупка ноутбука в рассрочку");
+
+int laptopPrice = 65000;
+int monthsCount = 12;
+double interestRate = 0.08;
+
+double totalWithInterest = laptopPrice * (1 + interestRate);
+double monthlyPayment = totalWithInterest / monthlyPayment;
+
+Console.WriteLine($"Цена ноутбука: {laptopPrice} руб.");
+Console.WriteLine($"Итого с процентами: {totalWithInterest} руб");
+Console.WriteLine($"Платёж в месяц: {monthlyPayment}");
+
+//деление
+Console.WriteLine();
 Console.WriteLine("Внимание: деление int");
 
 int totalStudents = 25;
