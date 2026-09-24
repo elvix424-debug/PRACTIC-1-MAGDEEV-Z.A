189db6d86828ee86b3674bc2260fa1fb4a55e239
elvix424-debug <elvix424@gmail.com>
2026-09-25 00:49:17 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,6 @@
+﻿double celsius = 34.2;
+const int F = 32;
+const double K = 273.15;
+Console.WriteLine($"Температура по Фаренгейту: {celsius * 9/5 + F}");
+Console.WriteLine($"Температура по Кельвину: {celsius + K}");
+Console.WriteLine($"{celsius}°C = {celsius * 9 / 5 + F}°F = {(int)((celsius + K) * 100) / 100.0}K");
